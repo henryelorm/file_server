ac734b8226bc50db1764ac7ea68c3ba606139ba9
Henry Quarshie <henryquarshie.hq@gmail.com>
2022-03-13 21:10:10 +0100

    Final stage

---
+++ .history/serve_20200720220928.py
@@ -0,0 +1,47 @@
+from flask import Flask, send_from_directory, render_template, request, redirect, url_for, safe_join
+import os
+
+app = Flask(__name__)
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# the directory files should be one level above the serve.py file
+# rel_dir = BASE_DIR + '/Lesson_Folder'
+
+rel_dir = 'e:/'
+
+
+
+hostip = '192.168.43.15'
+port = '5000'
+
+
+def list_files(directory):
+    files = []
+    for filename in os.listdir(directory):
+        path = os.path.join(directory, filename)
+        if os.path.isfile(path):
+            files.append(filename)
+        elif os.path.isdir(path):
+            files.append(filename)
+    return files
+
+
+@app.route('/')
+def index():
+    json_data = list_files(rel_dir)
+    return render_template('index.html', len=len(json_data), files=json_data)
+
+
+@app.route("/<path:path>")
+def get_file_folder(path):
+    location = safe_join(rel_dir, path)
+
+    if os.path.isfile(location):
+        return send_from_directory(rel_dir, path, as_attachment=True)
+
+    json_data = list_files(location)
+    return render_template('index.html', len=len(json_data), files=json_data)
+
+
+if __name__ == '__main__':
+    app.run(host=hostip, port=port, debug=True)
---
+++ README.md
@@ -0,0 +1,13 @@
+This is a simple service app that establishes a local http service to access directories that you want to share.
+
+
+---------------------------------------
+
+1.Change "hostip" in the "serve.py" located in the "Server" folder to your network IP address (ipconfig) in order to serve over local wireless network.
+
+2.customize the served directory.
+U can basically change the serving folder by re-assigning "rel_dir" to the customized path
+
+run command "python serve.py" to start server.
+
+note: make sure "Flask" is installed.
